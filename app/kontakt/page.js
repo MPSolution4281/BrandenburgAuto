f8c3a412e7e7a3c206ebe24d9d97d981e1cf596a
MPSolution4281 <mikkel546@gmail.com>
2026-08-24 09:39:40 +0200

    Add Google Business Profile map to contact page

--- app/kontakt/page.js
+++ app/kontakt/page.js
@@ -27,8 +27,22 @@ export default function ContactPage() {
         </div>
       </section>
       <section className="map-band">
-        <div><span className="kicker">Find værkstedet</span><h2>Bødkervej 9B<br />4300 Holbæk</h2></div>
-        <a className="button primary" href="https://www.google.com/maps/search/?api=1&query=Bødkervej+9B+4300+Holbæk" target="_blank" rel="noreferrer">Åbn rutevejledning</a>
+        <div className="map-band-copy">
+          <div><span className="kicker">Find værkstedet</span><h2>Bødkervej 9B<br />4300 Holbæk</h2></div>
+          <a className="button primary" href="https://www.google.com/maps/search/?api=1&query=Bødkervej+9B+4300+Holbæk" target="_blank" rel="noreferrer">Åbn rutevejledning</a>
+        </div>
+        <div className="map-frame">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2248.5890715341116!2d11.70911207674198!3d55.69613177306327!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc70cd05b641895d%3A0x3d86d174b41913c8!2sBrandenburg%20Autoservice%20-%20Autov%C3%A6rksted%20-%20Holb%C3%A6k!5e0!3m2!1sda!2sdk!4v1787556321884!5m2!1sda!2sdk"
+            title="Brandenburg Autoservice – Autoværksted i Holbæk på Google Maps"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="strict-origin-when-cross-origin"
+          />
+        </div>
       </section>
     </>
   );
